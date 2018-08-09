639ae4708d8f30ce7e6d8a1db48bd2a72e30f698
Bob Killen <rkillen@umich.edu>
2018-08-08 21:55:21 -0400

    add host check

--- skel/usr/lib/keepalived/scripts/chk_kube_apiserver.sh
+++ skel/usr/lib/keepalived/scripts/chk_kube_apiserver.sh
@@ -3,12 +3,14 @@
 vip="$1"
 port="$2"
 
+export HOST_ADDRESS="${HOST_ADDRESS:-localhost}"
+
  errorExit() {
      echo "*** $*" 1>&2
      exit 1
  }
 
- curl --silent --max-time 2 --insecure "https://localhost:$port/" -o /dev/null || errorExit "Error GET https://localhost:$port/"
+ curl --silent --max-time 2 --insecure "https://$HOST_ADDRESS:$port/" -o /dev/null || errorExit "Error GET https://$HOST_ADDRESS:$port/"
  if ip addr | grep -q "$vip"; then
      curl --silent --max-time 2 --insecure "https://$vip:$port/" -o /dev/null || errorExit "Error GET https://$vip:$port/"
  fi
